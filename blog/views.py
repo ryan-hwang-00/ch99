ecefc561fe9b814c0bf9d5ff91395201935a9032
ryan-hwang-00 <68258601+ryan-hwang-00@users.noreply.github.com>
2020-07-15 16:24:32 +0900

    commit 20200715 4PM 1/2

--- blog/views.py
+++ blog/views.py
@@ -48,9 +48,9 @@ class TaggedObjectLV(ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(tags_name=self.kwargs.get('tag'))
+        return Post.objects.filter(tags__name=self.kwargs.get('tag'))
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_gata(**kwargs)
+        context = super().get_context_data(**kwargs)
         context['tagname'] = self.kwargs['tag']
         return context
